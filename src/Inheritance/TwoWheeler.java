6f528207cb33c15bedf8aae7c96fec5b00f840d4
Rachit Saini <rachit.rrr2005@gmail.com>
2025-06-04 21:03:34 +0530

    Add files via upload

---
+++ src/Inheritance/TwoWheeler.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class TwoWheeler extends Vehicle{
+
+        public static void main(String[] args) {
+            Vehicle two = new Vehicle();
+            TwoWheeler vehicle = new TwoWheeler();
+            two.commute();
+            vehicle.commute();
+
+        }
+    }
+
