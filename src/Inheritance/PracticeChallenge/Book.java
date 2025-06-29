f0a2e75cd92108be843b8daa065958fe7609f716
Rachit Saini <rachit.rrr2005@gmail.com>
2025-06-29 23:58:30 +0530

    Challenge-1 (Inheritance)

---
+++ src/Inheritance/PracticeChallenge/Book.java
@@ -0,0 +1,13 @@
+package Inheritance.PracticeChallenge;
+
+public class Book extends LibraryItem{
+    private int isBnID;
+
+    public int getIsBnID() {
+        return isBnID;
+    }
+
+    public void setIsBnID(int isBnID) {
+        this.isBnID = isBnID;
+    }
+}
